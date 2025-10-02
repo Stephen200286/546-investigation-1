8d0f7768af0151fb60f086b5bd0734feb9479b83
Stephen Dumore <stephendumore115@gmail.com>
2025-10-01 21:21:27 -0700

    fixed httplogs

--- script.py
+++ script.py
@@ -11,13 +11,14 @@ def split_pcap_by_host(ips, pcap):
 def create_httplogs(ips, pcap):
     for ip in ips:
         cmd = "tcpdump -nn -r {pcap} host {ip} and port 80 -w httplogs/{ip}.pcap".format(pcap=pcap, ip=ip)
-        subprocess.Popen(cmd.split())
+        cmd = "tshark -r splits/{ip}.pcap -w httplogs/{ip}.pcap -Y 'http && ip.src == {ip}'".format(ip=ip)
+        subprocess.Popen(cmd, shell=True)
 
 #df = pd.read_csv("ips.csv", dtype="object")
 #split_pcap_by_host(df["IP"], "investigation1.pcap")
 
-#df = pd.read_csv("ShieldBase-Clients.csv", dtype="object")
-#create_httplogs(df["IP"], "investigation1.pcap")
+df = pd.read_csv("ShieldBase-Clients.csv", dtype="object")
+create_httplogs(df["IP"], "investigation1.pcap")
 
 df = pd.read_csv("ShieldBase-RD.csv", dtype="object")
 create_httplogs(df["IP"], "investigation1.pcap")
